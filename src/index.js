00678c788e88c9af0a6fd5b1df2994daf1b920dd
Talilotu <nesikaufusi@gmail.com>
2022-04-24 13:38:59 +1000

    Updated time

--- src/index.js
+++ src/index.js
@@ -1,24 +1,25 @@
-let currenttime = new Date();
+function formatDate(timestamp) {
+  let currentTime = new Date(timestamp);
 
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let day = days[currenttime.getDay()];
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[currentTime.getDay()];
 
-let minutes = currenttime.getMinutes();
-minutes = ("0" + minutes).slice(-2);
+  let hours = currentTime.getHours();
+  hours = ("0" + hours).slice(-2);
 
-let hours = currenttime.getHours();
-hours = ("0" + hours).slice(-2);
+  let minutes = currentTime.getMinutes();
+  minutes = ("0" + minutes).slice(-2);
 
-let currentdate = document.querySelector("#current-time");
-currentdate.innerHTML = `${day} ${hours}:${minutes}`;
+  return `${day} ${hours}: ${minutes}`;
+}
 
 function showWeather(response) {
   let temperature = Math.round(response.data.main.temp);
@@ -26,15 +27,18 @@ function showWeather(response) {
   let speed = Math.round(response.data.wind.speed);
 
   document.querySelector("h1").innerHTML = response.data.name;
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
   let celciusTemp = document.querySelector("#celciusTemp");
   celciusTemp.innerHTML = `${temperature}`;
   let humid = document.querySelector("#humidity");
   humid.innerHTML = `Humidity: ${humidity}%`;
-
   let wind = document.querySelector("#wind");
   wind.innerHTML = `Wind: ${speed}km/h`;
-  document.querySelector("#description").innerHTML =
-    response.data.weather[0].main;
+
+  let currentDate = document.querySelector("#current-time");
+  console.log(response);
+  currentDate.innerHTML = formatDate(response.data.dt * 1000);
 }
 function searchCity(city) {
   let apiKey = "f5098deaf242be7ff2c5af1ac2ce51ad";
