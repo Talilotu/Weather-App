808217d620afe9a7bf8738cf43e2dcb9bb1b9024
Talilotu <nesikaufusi@gmail.com>
2022-05-01 00:55:31 +1000

    Added Weather Forecast API

--- src/index.js
+++ src/index.js
@@ -21,7 +21,8 @@ function formatDate(timestamp) {
   return `${day} ${hours}: ${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = ``;
@@ -44,6 +45,15 @@ function displayForecast() {
 
   forecastElement.innerHTML = forecastHTML;
 }
+function getForecast(coordinates) {
+  console.log(coordinates);
+
+  let apiKey = "f5098deaf242be7ff2c5af1ac2ce51ad";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function showWeather(response) {
   celsiusTemperature = response.data.main.temp;
   windSpeed = Math.round(response.data.wind.speed);
@@ -72,6 +82,8 @@ function showWeather(response) {
     "src",
     `http://openweathermap.org/img/wn/${icon}@2x.png`
   );
+
+  getForecast(response.data.coord);
 }
 function searchCity(city) {
   let apiKey = "f5098deaf242be7ff2c5af1ac2ce51ad";
@@ -141,7 +153,5 @@ let celsiusButton = document.querySelector("#celsius-temp");
 celsiusButton.addEventListener("click", showCelsiusTemp);
 
 let celsiusTemperature = null;
-
-displayForecast();
 
 searchCity("New Zealand");
